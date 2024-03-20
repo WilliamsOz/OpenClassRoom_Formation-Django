5ee9f448d46862c3d0db8ef751469a60e7c04672
WilliamsOz <williams.ozserttas05@gmail.com>
2024-03-20 17:09:05 +0100

    Save

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
+from listings.models import Band
+# from listings.models import Title
+	# titles = Title.objects.all()
 from django.shortcuts import render
 
-def hello(request):
-	return HttpResponse('<h1>Hello Django!</h1>')
+def	hello(request):
+	bands = Band.objects.all()
+	return render(request, 'listings/hello.html', {'bands': bands})
+
+def	about(request):
+	return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def	listings(request):
+	return HttpResponse('<h1>COUCOU</h1>')
+
+def	contact(request):
+	return HttpResponse('<h1>CNS</h1>')
